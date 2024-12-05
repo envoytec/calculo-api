fdfd3e8f7e5eaba5d89f3b3cc73f2419fb70e3ab
kauecarmo <kauecarmo111@gmail.com>
2024-12-05 11:03:46 -0300

    Alterações Mínimas

---
+++ repositories/fileRepository.ts
@@ -0,0 +1,1 @@
+import { AppDataSource } from "../src/db/data-source";
---
+++ src/utils/fileUtils.ts
@@ -0,0 +1,16 @@
+const fs = require('fs')
+const path = require('path')
+
+export const ensureDirectory = (dirPath: string ) => {
+    if (!fs.existSync(dirPath)) {
+        fs.mkdirSync(dirPath, { recursive: true})
+    }
+};
+
+export const saveFile = (filePath: string, data: Buffer) => {
+    fs.writeFileSync(filePath, data)
+}
+
+export const getFilesDirectory = () => {
+    return path.join(__dirname, "C:\\Users\\kaue\\Desktop\\Tsc\\files\\")
+}
